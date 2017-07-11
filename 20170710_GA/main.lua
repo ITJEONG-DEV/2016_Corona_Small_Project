480f6ba3bc7869de97b4876b7824a79d08780e08
ITJEONG-NAN-JJANG <derbana1027@gmail.com>
2017-07-11 12:06:25 +0900

    버튼 추가
    버튼 추가

--- 20170710_GA/main.lua
+++ 20170710_GA/main.lua
@@ -1,6 +1,24 @@
+-- require modules
 local setMon = require "setMonster"
+local widget = require "widget"
 
-local compare
+local _W, _H = display.contentWidth, display.contentHeight
+
+local CC = function (hex)
+	local r = tonumber( hex:sub(1,2), 16 ) / 255
+	local g = tonumber( hex:sub(3,4), 16 ) / 255
+	local b = tonumber( hex:sub(5,6), 16 ) / 255
+	local a = 255 / 255
+
+	if #hex == 8 then a = tonumber( hex:sub(7,8), 16 ) / 255 end
+
+	return r, g, b, a
+end
+
+-- -----------------------------------------------------------------------------------
+-- basic settttting!
+-- -----------------------------------------------------------------------------------
+local compare, compete, competeB, exportB, textCount, bg
 
 -- stage count
 local count = 0
@@ -13,25 +31,31 @@ local user =
 	hp = 25,
 	atk = 25,
 	max = 25,
+	mp = 25,
 	weight = 50
 }
 
 local monData = {}
 
-
 function onStart( e )
-	-- setMonData
-	for i = 1, 20, 1 do
-		monData[i] = setMon.setMon(maxSum)
-		-- print(i.."번 째 몬스터.".." hp : "..monData[i].max.." atk : "..monData[i].atk)
-		compete(i)
-	end
+	if e.phase == "began" then
+		-- setMonData
+		for i = 1, 20, 1 do
+			monData[i] = setMon.setMon(maxSum)
+			-- print(i.."번 째 몬스터.".." hp : "..monData[i].max.." atk : "..monData[i].atk)
+			compete(i)
+		end
 
 
-	table.sort( monData, compare )
+		table.sort( monData, compare )
 
-	for i, v in pairs( monData ) do
-		print( i, v.score )
+		for i, v in pairs( monData ) do
+			print( i, v.score )
+		end
+
+		count = count + 1
+
+		textCount.text = "generatoin : "..count
 	end
 end
 
@@ -47,46 +71,131 @@ function compete( num )
 		end
 	end
 
-	for i = 1, 2, 1 do
-		flag = nil
+	flag = nil
 
-		user.hp = user.max
-		monData[num].hp = monData[num].max
+	monData[num].score = 0
+	user.hp = user.max
+	monData[num].hp = monData[num].max
 
-		-- user first
-		for turn = 1, 10, 1 do
-			-- print( num.."번 째 몬스터와 "..i.."번 째 전투 중 "..turn.."스테이지. 몬스터 : "..monData[num].hp .." 유저 : "..user.hp)
-			monData[num].hp = monData[num].hp - math.floor( user.atk * 0.2 )
-			-- print("몬스터에게 "..math.floor( user.atk * 0.2).."만큼의 데이지를 입혔습니다. 남은 몬스터의 체력 "..monData[num].hp)
+	-- user first
+	for turn = 1, 10, 1 do
+		-- print( num.."번 째 몬스터와 1 번 째 전투 중 "..turn.."스테이지. 몬스터 : "..monData[num].hp .." 유저 : "..user.hp)
 
-			user.hp = user.hp - math.floor( monData[num].atk * 0.2 )
-			-- print("몬스터가 "..math.floor( monData[num].atk * 0.2).."만큼의 데이지를 입혔습니다. 남은 체력 "..user.hp)
+		monData[num].hp = monData[num].hp - math.floor( user.atk * 0.2 )
+		-- print("몬스터에게 "..math.floor( user.atk * 0.2).."만큼의 데이지를 입혔습니다. 남은 몬스터의 체력 "..monData[num].hp)
 
-			if monData[num].hp <= 0 then
-				monData[num].score = getScore(1)
-				flag = true
-				break
-			elseif user.hp <= 0 then
-				monData[num].score = getScore(2)
-				flag = true
-				break
-			end
+		user.hp = user.hp - math.floor( monData[num].atk * 0.2 )
+		-- print("몬스터가 "..math.floor( monData[num].atk * 0.2).."만큼의 데이지를 입혔습니다. 남은 체력 "..user.hp)
+
+		if monData[num].hp <= 0 then
+			monData[num].score = monData[num].score + getScore(1)
+			flag = true
+			break
+		elseif user.hp <= 0 then
+			monData[num].score = monData[num].score + getScore(2)
+			flag = true
+			break
 		end
-
-		if not flag then
-			if monData[num].hp < user.hp then
-				monData[num].score = getScore(3)
-			elseif monData[num].hp > user.hp then
-				monData[num].score = getScore(4)
-			end
-		end
-
 	end
-	print(num.."번째 몬스터의 점수 : "..monData[num].score)
+
+	if not flag then
+		if monData[num].hp < user.hp then
+			monData[num].score = monData[num].score + getScore(3)
+		elseif monData[num].hp > user.hp then
+			monData[num].score = monData[num].score + getScore(4)
+		end
+	end
+
+	print(num.."번째 몬스터의 1 번 째 점수 : "..monData[num].score)
+
+	-- ------------------------------------------------------------
+
+	user.hp = user.max
+	monData[num].hp = monData[num].max
+
+		-- mon first
+	for turn = 1, 10, 1 do
+		-- print( num.."번 째 몬스터와 2 번 째 전투 중 "..turn.."스테이지. 몬스터 : "..monData[num].hp .." 유저 : "..user.hp)
+
+		user.hp = user.hp - math.floor( monData[num].atk * 0.2 )
+		-- print("몬스터가 "..math.floor( monData[num].atk * 0.2).."만큼의 데이지를 입혔습니다. 남은 체력 "..user.hp)
+
+		monData[num].hp = monData[num].hp - math.floor( user.atk * 0.2 )
+		-- print("몬스터에게 "..math.floor( user.atk * 0.2).."만큼의 데이지를 입혔습니다. 남은 몬스터의 체력 "..monData[num].hp)
+
+		if monData[num].hp <= 0 then
+			monData[num].score = monData[num].score +  getScore(1)
+			flag = true
+			break
+		elseif user.hp <= 0 then
+			monData[num].score = monData[num].score + getScore(2)
+			flag = true
+			break
+		end
+	end
+
+	if not flag then
+		if monData[num].hp < user.hp then
+			monData[num].score = monData[num].score + getScore(3)
+		elseif monData[num].hp > user.hp then
+			monData[num].score = monData[num].score + getScore(4)
+		end
+	end
+
+	print(num.."번째 몬스터의 2 번 째 점수 : "..monData[num].score/2)
 end
 
 function compare(a, b)
 	return a.score > b.score
 end
 
-onStart()
+function saveD(e)
+	if e.phase == "began" then
+	end
+end
+
+-- bg = display.newRect( 0, 0, _W, _H )
+-- bg:setFillColor( CC("ff6600") )
+-- bg.anchorX, bg.anchorY = 0, 0
+
+competeB = widget.newButton(
+    {
+        label = "경쟁하기",
+        onEvent = onStart,
+        fontSize = 15,
+        emboss = true,
+        -- Properties for a rounded rectangle button
+        shape = "roundedRect",
+        top = _H*0.85,
+        left = 10,
+        width = 100,
+        height = 30,
+        cornerRadius = 2,
+        labelColor = { default={ CC("666666") }, over={ CC("666666")} },
+        fillColor = { default={ CC("ffffff") }, over={ CC("888888")} },
+    }
+)
+
+exportB = widget.newButton(
+    {
+        label = "내보내기",
+        onEvent = saveD,
+        fontSize = 15,
+        emboss = true,
+        -- Properties for a rounded rectangle button
+        shape = "roundedRect",
+        top = _H*0.85,
+        left = 120,
+        width = 100,
+        height = 30,
+        cornerRadius = 2,
+        labelColor = { default={ CC("666666") }, over={ CC("666666")} },
+        fillColor = { default={ CC("ffffff") }, over={ CC("888888")} },
+    }
+)
+
+textCount = display.newText( "generation : "..count, _W*0.125, _H*0.775 )
+
+-- -----------------------------------------------------------------------------------
+-- event function listeners
+-- -----------------------------------------------------------------------------------
